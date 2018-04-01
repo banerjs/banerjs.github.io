06be9d074242ed7636f31326f98fe3c204264089
Siddhartha Banerjee <banerjs.sid@gmail.com>
2018-04-01 15:35:31 -0700

    Added a script to build latex files

---
+++ scripts/build_latex.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Build the latex files
+set -ev
+
+# Navigate to the pdf's directory
+cd pdf/
+
+# For each `.tex` file, build it
+# Nice to haves:
+#   a) specify a pdflatex/xelatex build
+#   b) specify .tex files to ignore
+#   c) suppress certain warnings from commands
+for texfile in *.tex; do
+    filename=$(basename ${texfile} .tex)
+    echo "Building " ${filename}
+    xelatex ${filename}.tex
+    bibtex ${filename}.aux || echo "Ignoring errors"
+    xelatex ${filename}.tex
+    xelatex ${filename}.tex
+done
