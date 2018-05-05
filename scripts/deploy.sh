de139d20829b9ec86458f746e5528b878df48b87
Siddhartha Banerjee <banerjs.sid@gmail.com>
2018-05-05 12:16:20 -0400

    Double -- suck

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="devel"
 TARGET_BRANCH="master"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
 fi
