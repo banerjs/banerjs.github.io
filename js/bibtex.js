6c34f4537275597753a7b9d93005ed448f51e743
Travis CI <banerjs@banerjs.com>
2020-07-27 03:50:54 +0000

    Deploy to GitHub Pages: ce1b31c71148df2ac8deb3bb0cc6263f01f72b60

--- js/bibtex.js
+++ js/bibtex.js
@@ -2,14 +2,15 @@
  * Custom javascript for bibtex interactions
  */
 
-function clicked_bib(element) {
+function clicked_bib(element_id) {
     // Find the bibtex child
     let bibtex = null;
-    for (let i = 0; i < element.children.length; i++) {
-        if (element.children[i].tagName.toUpperCase() === 'PRE') {
-            bibtex = element.children[i];
-        }
-    }
+    // for (let i = 0; i < element.children.length; i++) {
+    //     if (element.children[i].tagName.toUpperCase() === 'PRE') {
+    //         bibtex = element.children[i];
+    //     }
+    // }
+    bibtex = document.getElementById(element_id);
 
     // Don't do anything if the child was not found
     if (!bibtex) return;
