3eaa722ea86081638f352870dd45e4e28e7c5c0b
Siddhartha Banerjee <banerjs.sid@gmail.com>
2025-02-01 14:40:35 -0500

    chore: Update scripts to deploy

--- scripts/deploy_travis.sh
+++ scripts/deploy_travis.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-set -euo pipefail # Exit with nonzero exit code if anything fails
+set -ev # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="dev"
 TARGET_BRANCH="master"
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-CURRENT_CHANGES=$(git status --porcelain)
-CURRENT_SHA=$(git rev-parse --verify HEAD)
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$CURRENT_BRANCH" != "$SOURCE_BRANCH" ] || [ -n "$CURRENT_CHANGES" ]; then
-    echo -e "Skipping deploy because current branch is $CURRENT_BRANCH or changes exist:\n$CURRENT_CHANGES."
-    exit 1
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
 fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+
+# Now let's go have some fun with the cloned repo
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Copy the compiled stuff here
 rsync -avm \
@@ -48,7 +49,17 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
-git commit -m "Deploy to GitHub Pages: ${CURRENT_SHA}"
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../scripts/deploy_key.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
+eval `ssh-agent -s`
+ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
